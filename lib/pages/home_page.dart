0c9036f5aba6b603be42b2b46bf83a20321af008
Qudah03 <162819573+Qudah03@users.noreply.github.com>
2025-02-05 17:48:12 +0300

    added a box

--- lib/pages/home_page.dart
+++ lib/pages/home_page.dart
@@ -9,12 +9,28 @@ class HomePage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(title: const Text('Home Page')),
-      body: const SafeArea(
+      appBar: AppBar(
+        title: const Text(
+          'Home Page',
+        ),
+      ),
+      body: SafeArea(
         child: Stack(
           fit: StackFit.expand,
-          alignment: Alignment(0.0, 0.0),
-          children: [],
+          alignment: const Alignment(0.0, 0.0),
+          children: [
+            Positioned(
+              top: 108.0,
+              left: 33.0,
+              width: 360.0,
+              height: 214.0,
+              child: Container(
+                decoration: BoxDecoration(
+                    color: const Color(0xffc4c4c4),
+                    borderRadius: BorderRadius.circular(0.0)),
+              ),
+            )
+          ],
         ),
       ),
     );
