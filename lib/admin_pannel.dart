fe9515f58cb087a71fc844c129c45e0c69fe8dd4
Qudah03 <162819573+Qudah03@users.noreply.github.com>
2025-02-10 18:12:18 +0300

    added the admin pannel page

---
+++ lib/admin_pannel.dart
@@ -0,0 +1,35 @@
+import 'package:flutter/material.dart';
+import 'package:nowa_runtime/nowa_runtime.dart';
+
+@NowaGenerated()
+class AdminPannel extends StatefulWidget {
+  @NowaGenerated({'loader': 'auto-constructor'})
+  const AdminPannel({super.key});
+
+  @override
+  State<AdminPannel> createState() {
+    return _AdminPannelState();
+  }
+}
+
+@NowaGenerated()
+class _AdminPannelState extends State<AdminPannel> {
+  void func1() {}
+
+  void func() {}
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: SafeArea(
+        child: Stack(
+          fit: StackFit.expand,
+          alignment: const Alignment(0.0, 0.0),
+          textDirection: TextDirection.ltr,
+          clipBehavior: Clip.hardEdge,
+          children: [],
+        ),
+      ),
+    );
+  }
+}
